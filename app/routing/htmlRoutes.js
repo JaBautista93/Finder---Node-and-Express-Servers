752e0e555ee385015cdd83be92b88fbe72bec1ba
John-Anthony <41270598+JaBautista93@users.noreply.github.com>
2018-11-24 19:02:29 -0600

    route fix

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,12 +8,12 @@ module.exports = function(app) {
 
 
   app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/survey.html"));
+    res.sendFile(path.join(__dirname, "/app/public/survey.html"));
   });
 
   // No route is found, default to home
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/home.html"));
+    res.sendFile(path.join(__dirname, "/app/public/home.html"));
   });
 
 };
