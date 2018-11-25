8e262048804b6315b29c12e726f57feb549e05b1
John-Anthony <41270598+JaBautista93@users.noreply.github.com>
2018-11-24 18:49:23 -0600

    fixing typo errors

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,230 +1,217 @@
 // put all data in an array moron
-[
+var friends = [
     {
-    "name": "Pakalu Papito",
-    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCLjNZcZe1N1KMinVPNDxnbyitVd9lj-ypV_6Fr57MASQeXj3bOQ",
-    "scores": [
-    "5",
-    "1",
-    "4",
-    "4",
-    "5",
-    "1",
-    "2",
-    "5",
-    "4",
-    "1"
+    name: "Pakalu Papito",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCLjNZcZe1N1KMinVPNDxnbyitVd9lj-ypV_6Fr57MASQeXj3bOQ",
+    scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
     ]
-    },
-    {
-    "name": "Good Girl Gina",
-    "photo": "https://www.dailydot.com/wp-content/uploads/ab6/57/68d5c697a9a94f94032460c9980fd2e2.jpg",
-    "scores": [
-    "4",
-    "2",
-    "5",
-    "1",
-    "3",
-    "2",
-    "2",
-    "1",
-    "3",
-    "2"
+}, 
+{
+    name: "Good Girl Gina",
+    photo: "https://www.dailydot.com/wp-content/uploads/ab6/57/68d5c697a9a94f94032460c9980fd2e2.jpg",
+    scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
     ]
-    },
-    {
-    "name": "Bad Luck Brian",
-    "photo": "https://www.dailydot.com/wp-content/uploads/ae7/aa/8a85292ffb7c2178.png",
-    "scores": [
-    "5",
-    "2",
-    "2",
-    "2",
-    "4",
-    "1",
-    "3",
-    "2",
-    "5",
-    "5"
+}, {
+    name: "Bad Luck Brian",
+    photo: "https://www.dailydot.com/wp-content/uploads/ae7/aa/8a85292ffb7c2178.png",
+    scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
     ]
-    },
-    {
-    "name": "Doge",
-    "photo": "https://yt3.ggpht.com/a-/AN66SAw_M_11Alp1TIDompdqcTC94F-T-BUKgNgSFw=s900-mo-c-c0xffffffff-rj-k-no",
-    "scores": [
-    "3",
-    "3",
-    "4",
-    "2",
-    "2",
-    "1",
-    "3",
-    "2",
-    "2",
-    "3"
+}, {
+    name: "Doge",
+    photo: "https://yt3.ggpht.com/a-/AN66SAw_M_11Alp1TIDompdqcTC94F-T-BUKgNgSFw=s900-mo-c-c0xffffffff-rj-k-no",
+    scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
     ]
-    },
-    {
-    "name": "Fart Guy",
-    "photo": "https://img.memecdn.com/hold-fart-intensifies_fb_2915237.jpg",
-    "scores": [
-    "4",
-    "3",
-    "4",
-    "1",
-    "5",
-    "2",
-    "5",
-    "3",
-    "1",
-    "4"
+}, {
+    name: "Fart Guy",
+    photo: "https://img.memecdn.com/hold-fart-intensifies_fb_2915237.jpg",
+    scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
     ]
-    },
-    {
-    "name": "Ridiculously Photogenic Guy",
-    "photo": "https://i.dailymail.co.uk/i/pix/2012/04/06/article-2125879-1276A7B0000005DC-166_634x358.jpg",
-    "scores": [
-    "4",
-    "4",
-    "2",
-    "3",
-    "2",
-    "2",
-    "3",
-    "2",
-    "4",
-    "5"
+}, {
+    name: "Ridiculously Photogenic Guy",
+    photo: "https://i.dailymail.co.uk/i/pix/2012/04/06/article-2125879-1276A7B0000005DC-166_634x358.jpg",
+    scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
     ]
-    },
-    {
-    "name": "Overely Attached Girlfriend",
-    "photo": "http://giggag.com/thumbnails/201808/15336288158.jpg",
-    "scores": [
-    "3",
-    "3",
-    "3",
-    "2",
-    "4",
-    "4",
-    "2",
-    "3",
-    "3",
-    "3"
+}, {
+    name: "Overely Attached Girlfriend",
+    photo: "http://giggag.com/thumbnails/201808/15336288158.jpg",
+    scores: [
+        "3",
+        "3",
+        "3",
+        "2",
+        "4",
+        "4",
+        "2",
+        "3",
+        "3",
+        "3"
     ]
-    },
-    {
-    "name": "Jeff",
-    "photo": "instagram.com",
-    "scores": [
-    "5",
-    "5",
-    "5",
-    "5",
-    "5",
-    "5",
-    "5",
-    "5",
-    "5",
-    "5"
+}, {
+    name: "Jeff",
+    photo: "instagram.com",
+    scores: [
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5"
     ]
-    },
-    {
-    "name": "Good Luck Charlie",
+}, {
+    name: "Good Luck Charlie",
     "photo": "http://giggag.com/thumbnails/201808/15336288122.jpg",
     "scores": [
-    "3",
-    "1",
-    "4",
-    "4",
-    "4",
-    "2",
-    "3",
-    "3",
-    "5",
-    "4"
+        "3",
+        "1",
+        "4",
+        "4",
+        "4",
+        "2",
+        "3",
+        "3",
+        "5",
+        "4"
     ]
-    },
-    {
-    "name": "Side-Eyeing Chloe",
-    "photo": "http://giggag.com/thumbnails/201808/15336288092.jpg",
-    "scores": [
-    "3",
-    "1",
-    "4",
-    "4",
-    "4",
-    "2",
-    "3",
-    "3",
-    "5",
-    "4"
+}, {
+    name: "Side-Eyeing Chloe",
+    photo: "http://giggag.com/thumbnails/201808/15336288092.jpg",
+    scores: [
+        "3",
+        "1",
+        "4",
+        "4",
+        "4",
+        "2",
+        "3",
+        "3",
+        "5",
+        "4"
     ]
-    },
-    {
-    "name": "Disaster Girl",
-    "photo": "http://giggag.com/thumbnails/201808/15336288054.jpg",
-    "scores": [
-    "3",
-    "1",
-    "4",
-    "4",
-    "4",
-    "2",
-    "3",
-    "3",
-    "5",
-    "4"
+}, {
+    name: "Disaster Girl",
+    photo: "http://giggag.com/thumbnails/201808/15336288054.jpg",
+    scores: [
+        "3",
+        "1",
+        "4",
+        "4",
+        "4",
+        "2",
+        "3",
+        "3",
+        "5",
+        "4"
     ]
-    },
-    {
-    "name": "Success Kid",
-    "photo": "http://giggag.com/thumbnails/201808/15336288029.jpg",
-    "scores": [
-    "3",
-    "1",
-    "4",
-    "4",
-    "4",
-    "2",
-    "3",
-    "3",
-    "5",
-    "4"
+}, {
+    name: "Success Kid",
+    photo: "http://giggag.com/thumbnails/201808/15336288029.jpg",
+    scores: [
+        "3",
+        "1",
+        "4",
+        "4",
+        "4",
+        "2",
+        "3",
+        "3",
+        "5",
+        "4"
     ]
-    },
-    {
-    "name": "Advice Giving Morpheous",
-    "photo": "https://www.timecamp.com/blog/wp-content/uploads/2018/02/work-memes-1.jpg",
-    "scores": [
-    "3",
-    "1",
-    "2",
-    "3",
-    "4",
-    "3",
-    "3",
-    "3",
-    "4",
-    "4"
+}, {
+    name: "Advice Giving Morpheous",
+    photo: "https://www.timecamp.com/blog/wp-content/uploads/2018/02/work-memes-1.jpg",
+    scores: [
+        "3",
+        "1",
+        "2",
+        "3",
+        "4",
+        "3",
+        "3",
+        "3",
+        "4",
+        "4"
     ]
-    },
-    {
-    "name": "John Bautista",
-    "photo": "https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/14068315_10153886387803652_6201336513015341946_n.jpg?_nc_cat=102&_nc_ht=scontent-dfw5-2.xx&oh=2c214d65289144168edf9594da0a11ce&oe=5C690817",
-    "scores": [
-    "5",
-    "5",
-    "5",
-    "5",
-    "5",
-    "5",
-    "5",
-    "5",
-    "5",
-    "5"
+}, {
+    name: "John Bautista",
+    photo: "https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/14068315_10153886387803652_6201336513015341946_n.jpg?_nc_cat=102&_nc_ht=scontent-dfw5-2.xx&oh=2c214d65289144168edf9594da0a11ce&oe=5C690817",
+    scores: [
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5"
     ]
-    }
-    ];
-    
-    //exports data to the app
-    module.exports = friends;
+}];
+
+//exports data to the app
+module.exports = friends;
